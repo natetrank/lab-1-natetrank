671e3ae862e3d348273774ca3207cf4414acb557
natetrank <ntrank@calpoly.edu>
2017-04-05 12:34:59 -0700

    1 & 2

--- lab1.py
+++ lab1.py
@@ -1,14 +1,13 @@
 #* Section 1 (Git)
 # persnickety
-
 #* Section 2 (Data Definitions)
 
 #* 1)
-
+# input: celsius_temp is a float, result: fahrenheit_temp is a float
 #* 2)
-
+# price is represented as a float
 #* 3)
-
+#
 #* 4)
 
 #* Section 3 (Signature, Purpose Statements, Headers)
